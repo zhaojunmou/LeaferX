6757cbc3a647aad7e58137828dc1b5ff6f344420
zhaojunmou <35146855+zhaojunmou@users.noreply.github.com>
2024-02-07 08:11:03 +0000

    改了一下端口

--- main.ts
+++ main.ts
@@ -1,82 +1,105 @@
-import { Rect , App , Text , Line} from 'leafer-ui'
+import { Rect, App, Text, Line, Box, PointerEvent, Group } from 'leafer-ui'
 import { EditorMoveEvent } from './src/event/EditorMoveEvent'
 import { EditorEvent } from './src/event/EditorEvent'
 import { Editor } from './src' // 引入插件代码
 
-
-// const leafer = new Leafer({ view: window })
-const app = new App({ 
-    view: window
+const app = new App({
+  view: window,
 })
 
 app.tree = app.addLeafer()
 app.sky = app.addLeafer({ type: 'draw', usePartRender: false })
 
-const editor = app.editor = new Editor()
-app.sky.add(app.editor)
+const editor = (app.editor = new Editor())
+
+const creator = new Group({
+  visible: false,
+  children: [
+    {
+      tag: 'Rect',
+      width: app.width,
+      height: app.height,
+      fill: 'rgba(0, 0, 0, 0.5)',
+    },
+  ],
+})
+
+app.sky.addMany(editor, creator)
 
 const rect1 = new Rect({
-    x: 100,
-    y: 100,
-    width: 100,
-    height: 100,
-    fill: '#32cd79',
-    editable: true
+  x: 100,
+  y: 100,
+  width: 100,
+  height: 100,
+  fill: '#32cd79',
+  editable: true,
 })
 const rect2 = new Rect({
-    x: 150,
-    y: 150,
-    width: 100,
-    height: 100,
-    fill: '#7932cd',
-    editable: true
+  x: 150,
+  y: 150,
+  width: 100,
+  height: 100,
+  fill: '#7932cd',
+  editable: true,
 })
 
 const line1 = new Line({
-    points:[30,10,50,36,70,10,112,60],
-    curve:0.5,
-    strokeWidth:3,
-    stroke: '#3279cd',
-    editable: true
+  points: [30, 10, 50, 36, 70, 10, 112, 60],
+  curve: 0.5,
+  strokeWidth: 3,
+  stroke: '#3279cd',
+  editable: true,
 })
 
 const line2 = new Line({
-    x:100,
-    y:80,
-    width:200,
-    strokeWidth:3,
-    stroke: '#9c327d',
-    editable: true
+  x: 100,
+  y: 80,
+  width: 200,
+  strokeWidth: 3,
+  stroke: '#9c327d',
+  editable: true,
 })
 
 const x = new Text({
-    text:'x:'
+  text: 'x:',
 })
 const y = new Text({
-    text:'y:',
-    y:20
+  text: 'y:',
+  y: 20,
+})
+
+const button = new Box({
+  x: 500,
+  y: 40,
+  fill: 'blue',
+  cornerRadius: 20,
+  children: [
+    {
+      tag: 'Text',
+      text: 'button',
+      padding: [8, 14],
+      fill: '#ffffff',
+    },
+  ],
 })
 
 const selectCount = new Text({
-    text:'count:',
-    y:40
+  text: 'count:',
+  y: 40,
 })
 
-editor.on(EditorMoveEvent.MOVE,(e)=>{
-    x.text = 'x:' + e.target.x
-    y.text = 'y:' + e.target.y
+button.on(PointerEvent.DOWN, () => {
+  console.log('click button')
+  creator.visible = !creator.visible
 })
 
-editor.on(EditorEvent.SELECT,(e)=>{
-    selectCount.text = 'count:' + e.editor.list.length
-    console.log(e.value.tag)
+editor.on(EditorMoveEvent.MOVE, (e) => {
+  x.text = 'x:' + e.target.x
+  y.text = 'y:' + e.target.y
 })
 
-app.tree.addMany(rect1,rect2,line1,line2,x,y,selectCount)
-// app.tree.add(rect2)
-// app.tree.add(line)
-// app.tree.add(x)
-// app.tree.add(y)
-// app.tree.add(selectCount)
+editor.on(EditorEvent.SELECT, (e) => {
+  selectCount.text = 'count:' + e.editor.list.length
+})
 
-// console.log( new Editor() ) 
+app.tree.addMany(rect1, rect2, line1, line2, x, y, selectCount, button)
